2eabfeab72bbbb15adfb8c976fb3a7d5cdf8ffd9
Adam Sauer <adamsauer.ee@gmail.com>
2017-10-08 17:28:43 -0400

    adding updated unity code with IOT integration

---
+++ unityCode/Assets/Receive.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+
+public class Receive : MonoBehaviour {
+
+    SerialPort stream = new SerialPort("COM7", 115200);
+    private char[] delimeters = { ',', '\n' };
+    public static string humidity;
+    public static string temperature;
+    public static int RFID;
+
+    public static bool RFID1;
+    public static bool RFID2;
+    public static bool RFID3;
+
+    public static bool allPlayers;
+
+    private float timer;
+
+    private string receivedData;
+
+    // Use this for initialization
+    void Start() {
+        stream.ReadTimeout = 50;
+        stream.Open();
+
+        RFID1 = false;
+        RFID2 = false;
+        RFID3 = false;
+        allPlayers = false;
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        receivedData = stream.ReadLine();
+        if(receivedData != null)
+        {
+            Debug.Log("Data is not null right now");
+            Debug.Log("receivedData is " + receivedData);
+            parseData(receivedData);
+        }
+        else
+        {
+            Debug.Log("Data is null");
+        }
+
+        //if (allPlayers)
+        //{
+
+            //Todo: create experience
+            //Debug.Log("CREATE EXPERIENCE");
+        //}
+    }
+
+    void parseData(string receivedData)
+    {
+        string[] data = receivedData.Split(delimeters);
+        if(data.Length == 2) //humidity, temperature
+        {
+            humidity = data[0];
+            temperature = data[1];
+            RFID = 0; 
+        } else if (data.Length == 3) //humidity, temperature, RFID
+        {
+            humidity = data[0];
+            temperature = data[1];
+            RFID = int.Parse(data[2]);
+            if (RFID == 1 && !RFID1)
+            {
+                RFID1 = true;
+                checkAllPlayers();
+                Debug.Log("RFID 1 REGISTERED");
+            } else if (RFID == 2 && !RFID2)
+            {
+                RFID2 = true;
+                checkAllPlayers();
+                Debug.Log("RFID 2 REGISTERED");
+            } else if (RFID == 3 && !RFID3)
+            {
+                RFID3 = true;
+                checkAllPlayers();
+                Debug.Log("RFID 3 REGISTERED");
+            } else
+            {
+                Debug.Log("error: RFID device not supported");
+            }
+
+            Debug.Log("Humidty is: " + humidity);
+            Debug.Log("Temp is: " + temperature);
+            Debug.Log("RFID 1: " + RFID1);
+            Debug.Log("RFID 2: " + RFID2);
+            Debug.Log("RFID 3: " + RFID3);
+        }
+        else
+        {
+            Debug.Log("error parsing Arduino data stream");
+            Debug.Log(data.Length);
+        }
+    }
+
+    void checkAllPlayers()
+    {
+        if (RFID1 && RFID1 && RFID3)
+        {
+            allPlayers = true;
+        }
+    }
+
+    public void clearAllPlayers()
+    {
+        RFID1 = false;
+        RFID2 = false;
+        RFID3 = false;
+        allPlayers = false;
+    }
+}
---
+++ unityCode/Assets/ToggleLayer.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Rogue Fong, 8 October 2017
+
+public class ToggleLayer : MonoBehaviour {
+
+    public enum Layer { Municipal, PopupExperience };
+
+    public static int layer;
+    public static int numOfLayers = 3;
+
+    public GameObject[] municipalMembers;
+    public GameObject[] popupExperienceMembers;
+
+    //layer 0: empty, layer 1: municipal, layer 2: popup experience
+
+	// Use this for initialization
+	void Start () {
+        layer = 0;
+
+        //if(municipalMembers == null)
+        //{
+            municipalMembers = GameObject.FindGameObjectsWithTag("MunicipalMember");
+            foreach (GameObject member in municipalMembers)
+            {
+                Debug.Log(member);
+                member.transform.SetParent(gameObject.transform);
+            }
+        //}
+        //if(popupExperienceMembers == null)
+        //{
+            popupExperienceMembers = GameObject.FindGameObjectsWithTag("PopupExperienceMember");
+            foreach (GameObject member in popupExperienceMembers)
+            {
+                Debug.Log(member);
+                member.transform.SetParent(gameObject.transform);
+            }
+        //}
+        Debug.Log("Layer is " + layer);
+        AutoToggle();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            Debug.Log("tab pressed");
+            Debug.Log("Layer was " +  layer);
+            changeLayer();
+            Debug.Log("Layer now is " + layer);
+
+            AutoToggle();
+        }
+		
+	}
+
+
+    void ToggleMunicipalOn()
+    {
+        foreach (GameObject member in municipalMembers)
+        {
+            member.SetActive(true);
+        }
+    }   
+
+    void ToggleMunicipalOff()
+    { 
+        foreach(GameObject member in municipalMembers)
+        {
+            member.SetActive(false);
+        }
+     }
+
+    void TogglePopupExperienceOn()
+    {
+            foreach(GameObject member in popupExperienceMembers)
+            {
+                member.SetActive(true);
+            }
+    }
+
+    void TogglePopupExperienceOff()
+    {
+        foreach (GameObject member in popupExperienceMembers)
+        {
+            member.SetActive(false);
+        }
+    }
+
+    void changeLayer() //increment through layers
+    {
+        Debug.Log("Called changeLayer()");
+        if (layer >= numOfLayers - 1)
+        {
+            layer = 0;
+        }
+        else
+        {
+            layer++;
+        }
+    }
+
+    void AutoToggle()
+    {
+        Debug.Log("Toggling Layer");
+        if (layer == 0)
+        {
+            //isMunicipal = false;
+            //isPopupExperience = false;
+            ToggleMunicipalOff();
+            TogglePopupExperienceOff();
+            Debug.Log("Everything should be off");
+        }
+        else if (layer == 1)
+        {
+            ToggleMunicipalOn();
+            TogglePopupExperienceOff();
+            Debug.Log("Municipal should be on, whilst popup should all be off.");
+        }
+        else if (layer == 2)
+        {
+            ToggleMunicipalOff();
+            TogglePopupExperienceOn();
+            Debug.Log("Municipal should all be off, whilst popup should all be on.");
+        }
+        else
+        {
+            Debug.Log("error: layer does not exist");
+            layer = 0;
+            ToggleMunicipalOff();
+            TogglePopupExperienceOff();
+        }
+    }
+}
